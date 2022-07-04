547a63c77d0d6bc73f495d4f7355e29bd9436ea0
Uriel Soubran <uriel_bee15@hotmail.com>
2022-07-04 02:34:45 -0500

    Inicializacion con vite
    npm init vite@latest

---
+++ Udemy-JuanPablo/Proyecto1-AdminPacientes/citas-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ListadoPacientes from './components/ListadoPacientes'
+
+const App = () => {
+  return (
+    <div>
+      App
+      <ListadoPacientes />
+    </div>
+  )
+}
+
+export default App
