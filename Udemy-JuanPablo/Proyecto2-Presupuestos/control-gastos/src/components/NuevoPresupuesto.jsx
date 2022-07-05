f727f3aa3b984d5186a3e0ac000c9252f0f4937b
Uriel Soubran <uriel_bee15@hotmail.com>
2022-07-05 06:45:42 -0500

    Add state isValidPresupuesto to handle switch between components
    1- App add state isValidPresupuesto and forward it to Header -> NuevoPresupuesto
    
    2- In Header, added ternary to check if isValid is true or false, and then render a component or another.

--- Udemy-JuanPablo/Proyecto2-Presupuestos/control-gastos/src/components/NuevoPresupuesto.jsx
+++ Udemy-JuanPablo/Proyecto2-Presupuestos/control-gastos/src/components/NuevoPresupuesto.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import Mensaje from "./Mensaje";
 
-const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+const NuevoPresupuesto = ({
+  presupuesto,
+  setPresupuesto,
+  setIsValidPresupuesto,
+}) => {
   const [mensaje, setMensaje] = useState("");
 
   const handlePresupuesto = (e) => {
@@ -9,10 +13,14 @@ const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
     //Aceptar un numero => Number(state)
     //If not Number => NaN
 
-    if (!Number(presupuesto) || Number(presupuesto) < 0) {
+    //As the input changed from text to number, we no longer need to convert Number(state)
+    if (!presupuesto || presupuesto < 0) {
       setMensaje("No es un presupuesto válido.");
       return;
     }
+
+    setMensaje("");
+    setIsValidPresupuesto(true);
   };
 
   return (
@@ -21,11 +29,11 @@ const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
         <div className="campo">
           <label htmlFor="">Definir Presupuesto</label>
           <input
-            type="text"
+            type="number"
             className="nuevo-presupuesto"
             placeholder="Añade tu presupuesto"
             value={presupuesto}
-            onChange={(e) => setPresupuesto(e.target.value)}
+            onChange={(e) => setPresupuesto(Number(e.target.value))}
           />
         </div>
         <input type="submit" value="Añadir" />
