b931217fbaac3af66883047f0b91d1ce795a4135
Uriel Soubran <uriel_bee15@hotmail.com>
2022-07-29 03:34:59 -0500

    About Page
    So ... it works a bit different than React SPA alone.
    
    It has a router built in with file directory structure, so if you want to go to mydomain.com/about
    you would only need to make a file about.js or about.jsx in the same directory than index.js
    Something like this:
    
    pages
    | api
    | _app.js
    | index.js
    | about.jsx
    
    Note that in React we often write the filename capitalized, like About.jsx when referring to a component.
    In Next, that is not the case, because the filename corresponds to the route that will be written on the search bar (forgot the name, the one where you write the web domain, at the top of the browser)
    
    So, if you want to go to mydomain.com/some-page
    The component needs to be called some-page.jsx, not Some-page.jsx
    
    Also, the name of the component can have nothing to do with the filename.
    So, in about.jsx my component is called AboutPage.

---
+++ Next/01-init-demo/pages/about.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+const AboutPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>About - Soubran</title>
+        <meta name="description" content="About Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Ir a <a href="/">Home</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{" "}
+          <code className={styles.code}>pages/about.jsx</code>
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default AboutPage;
