527b026ac51160b5c50637bdfa27a343b5ea96e4
Uriel Soubran <uriel_bee15@hotmail.com>
2022-07-29 06:01:13 -0500

    Dynamic Navbar
    Its pretty much a semi dynamic thing, just because the array is already written in the file.
    
    But it works just fine.

--- Next/01-init-demo/components/Navbar.jsx
+++ Next/01-init-demo/components/Navbar.jsx
@@ -1,14 +1,32 @@
 import React from "react";
-import Link from "next/link";
 import styles from "./Navbar.module.css";
 import ActiveLink from "./ActiveLink";
+
+const menuItems = [
+  {
+    text: "Home",
+    href: "/",
+  },
+  {
+    text: "About",
+    href: "/about",
+  },
+  {
+    text: "Contact",
+    href: "/contact",
+  },
+  {
+    text: "Pricing",
+    href: "/pricing",
+  },
+];
 
 const Navbar = () => {
   return (
     <nav className={styles["menu-container"]}>
-      <ActiveLink text="Home" href="/" />
-      <ActiveLink text="About" href="/about" />
-      <ActiveLink text="Contact" href="/contact" />
+      {menuItems.map(({ text, href }, index) => (
+        <ActiveLink text={text} href={href} key={index} />
+      ))}
     </nav>
   );
 };
