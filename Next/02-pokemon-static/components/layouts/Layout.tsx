239797794a4b9e7447885d693fb39af871875230
Uriel Soubran <uriel_bee15@hotmail.com>
2022-07-29 11:27:02 -0500

    Layout and Props interface
    Interface to receive children and dynamic title for the Head.
    
    Doc:
    - https://www.pluralsight.com/guides/defining-props-in-react-function-component-with-typescript

---
+++ Next/02-pokemon-static/components/layouts/Layout.tsx
@@ -0,0 +1,26 @@
+import { FC, PropsWithChildren } from "react"
+
+import Head from "next/head"
+
+interface Props {
+    title?: string;
+}
+  
+
+export const Layout: FC<PropsWithChildren<Props>> = ({children, title}) => {
+  return (
+    <>
+        <Head>
+            <title>{title || 'Pokemon App'}</title>
+            <meta name='author' content='Uriel Soubran' />
+            <meta name='description' content={`Información sobre el pokemon ${title}`} />
+            <meta name='keywords' content={`${title}, pokemon, pokedex`} />
+        </Head>
+
+        {/* <Navbar /> */}
+        <main>
+            {children}
+        </main>
+    </>
+  )
+}
