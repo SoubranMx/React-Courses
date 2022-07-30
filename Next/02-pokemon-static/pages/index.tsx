c1f1b2c12abe292b0360447385cc58b80b09454e
Uriel Soubran <uriel_bee15@hotmail.com>
2022-07-30 08:37:19 -0500

    IMPORTANT: getStaticProps
    It's a bit hard to get it 1st time.
    
    If you ever forget what this is about, see the video
    
    Section 04: 45. Next - GetStaticProps
    
    
    So, how to say it. This function only works for Pages, not for basic components.
    The function runs in build time, when you yarn build.
    This is ahead of time as when some user goes to the page, so the function it's already done by the time it's deployed on internet.
    
    Here you can do a lot of things that are commonly used inside a server, like sending http request, read the filesystem, read databases, etc.
    All in order to inject that information to the Page (in this case, only HomePage).
    i.e. Get ALL the pokemons from the pokeapi.co, so to be rendered in a grid layout or something.
    Or so I think this is going to be used for later on the course.
    
    This, of course, in React you could do it with a useEffect hook, but that will work ONCE the user enters the page, it will be doing it once per user request.
    Let's say the API doesn't work some day, you're screwed.
    With getStaticProps, the API works fine TODAY, when we build it. Then it will inject the info that an useEffect could do, but generates the pages ahead of time, and it will only do it ONCE. Just for the build time, not for every user request.

--- Next/02-pokemon-static/pages/index.tsx
+++ Next/02-pokemon-static/pages/index.tsx
@@ -1,16 +1,51 @@
-import { Button } from '@nextui-org/react'
-import type { NextPage } from 'next'
+import { NextPage, GetStaticProps } from 'next'
+
 import { Layout } from '../components/layouts'
+import { Button } from '@nextui-org/react'
 
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log({props})
   return (
     <Layout title='Listado de Pokemons'>
-      <Button color='gradient'>
-        Hola Mundo
-      </Button>
+      <ul>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+      </ul>
     </Layout>
   )
+}
+
+/*
+  Generalmente ponemos el staticProps abajo del componente que la usa
+  Las props que se definen aquí, van a ser pasadas como props al componente (HomePage en este caso)
+  
+  GetStaticProps se ejecuta del lado del servidor y SOLO SE EJECUTA en build time (yarn build). Eso y en yarn dev.
+  Solo pueden usarse dentro de Pages, no en cualquier componente.
+  Es decir, en esa función puede leerse el filesystem, peticiones http con secret tokens, base de datos, etc. Cosas que suelen ser del lado del servidor.
+  Nada llega al cliente, con excepcion de las props
+  
+  El siguiente comentario viene cuando se usa el snippet nextgetStaticProps
+*/
+
+// You should use getStaticProps when:
+//- The data required to render the page is available at build time ahead of a user’s request.
+//- The data comes from a headless CMS.
+//- The data can be publicly cached (not user-specific).
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  //const { data } = await  // your fetch function here 
+  console.log('Hola mundo staticProps') //Este clg aparece en la consola (Terminal), no en la consola de Chrome/Edge/Firefox
+
+  return {
+    props: {
+      
+    }
+  }
 }
 
 export default HomePage
