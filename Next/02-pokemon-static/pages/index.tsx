5afc92ab66bd3aef6f76cd1701d8b620cca9ffa4
Uriel Soubran <uriel_bee15@hotmail.com>
2022-07-30 09:30:48 -0500

    Interfaces, API and SSG
    The api folder that is in root, IT'S NOT THE API FOLDER from pages. That one is the endpoint on Next.
    This api folder on root, is for axios.
    
    Too many changes, forgot to commit on interfaces.
    
    Installed axios too.
    
    So, we began by request to pokeapi 151 pokemons.
    Each request, stored in data, is of type PokemonListResponse, which we created an interface with Postman and PasteJSONasCode.
    
    That interface has some errors by default, mainly previous, the extension thinks previous is always null, which is not. So, previous?: string;
    Also, renamed results type as SmallPokemon[]
    
    SmallPokemon has, by default, only 2 properties: name and url.
    We added id and img, because we wanted to have an id (later on used within li key) and a svg image path.
    
    Those properties that does not come with the axios response, we added them manually with the map function that already knows that it is an array because of the interface.
    
    Once you have the new array, you pass them as props to HomePage, and there you can also use the map function (again, because TS knows it's an array) and render 151 pokemons.
    
    When you make a yarn build, it generates a JSON with all those objects: {name, url, id, img} so when you use yarn start, you'll no longer request anything to pokeapi.co each time you load the page, because it already did it for you with staticProps.
    
    That is called Static Site Generation (SSG).
    That's the magic about it.

--- Next/02-pokemon-static/pages/index.tsx
+++ Next/02-pokemon-static/pages/index.tsx
@@ -1,19 +1,27 @@
 import { NextPage, GetStaticProps } from 'next'
+import { pokeApi } from '../api'
 
 import { Layout } from '../components/layouts'
 import { Button } from '@nextui-org/react'
+import { PokemonListResponse, SmallPokemon } from '../interfaces'
 
 
-const HomePage: NextPage = (props) => {
-  console.log({props})
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const HomePage: NextPage<Props> = ({pokemons}) => {
+  console.log(pokemons)
   return (
     <Layout title='Listado de Pokemons'>
       <ul>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
-        <li>Pokémon</li>
+        {
+          pokemons.map(({id, name}) => (
+            <li key={ id }>
+              #{ id } - { name }
+            </li>
+          ))
+        }
       </ul>
     </Layout>
   )
@@ -27,7 +35,7 @@ const HomePage: NextPage = (props) => {
   Solo pueden usarse dentro de Pages, no en cualquier componente.
   Es decir, en esa función puede leerse el filesystem, peticiones http con secret tokens, base de datos, etc. Cosas que suelen ser del lado del servidor.
   Nada llega al cliente, con excepcion de las props
-  
+
   El siguiente comentario viene cuando se usa el snippet nextgetStaticProps
 */
 
@@ -39,11 +47,20 @@ const HomePage: NextPage = (props) => {
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   //const { data } = await  // your fetch function here 
-  console.log('Hola mundo staticProps') //Este clg aparece en la consola (Terminal), no en la consola de Chrome/Edge/Firefox
+  //console.log('Hola mundo staticProps') //Este clg aparece en la consola (Terminal), no en la consola de Chrome/Edge/Firefox
+  
+  const {data} = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
+  const pokemons: SmallPokemon[] = data.results.map((poke, index) => ({
+      ...poke,
+      id: index + 1,
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ index + 1 }.svg`
+  }))
+
+  //https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/id.svg
 
   return {
     props: {
-      
+      pokemons
     }
   }
 }
