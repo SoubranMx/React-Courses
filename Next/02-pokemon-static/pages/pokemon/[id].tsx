e1b6b4945484021e670f50737a56ec61dc775743
Uriel Soubran <uriel_bee15@hotmail.com>
2022-07-30 10:56:03 -0500

    IMPORTANT: getStaticPaths
    Woooh, there's a lot of things to understand here.
    
    Again, see the video Section 04: 52. Next - getStaticPaths and 53. Generando las 151 páginas de Pokémons
    
    Where to even begin?
    
    You write the getStaticPaths function before the getStaticProps. Not sure if it NEEDS to be before, but anyway.
    
    GetstaticPaths is the one in charge to create ALL 151 PAGES when yarn build.
    
    Later on, getStaticPaths allows StaticProps to obtain the params on the url, in this case, just the id as a string, using the context.
    The context has way more properties, but we destructure it to obtain only params.
    
    From the params, we obtain the id, and with that ID we axios the shit out of the pokemon ID.
    
    We send that data as props to the page and there you can do whatever the hell you want with it.
    
    In dev mode it is slow af, but when you yarn build, the build process is the one and only time it will be slow, because it is fetching the data and stuff from all 151 pokemons.
    
    Once it finishes, yarn start will serve all the pages and it will be smooth fast... tho, not as fast as simple React App with vite or CRA.

--- Next/02-pokemon-static/pages/pokemon/[id].tsx
+++ Next/02-pokemon-static/pages/pokemon/[id].tsx
@@ -1,17 +1,62 @@
+import { NextPage, GetStaticProps, GetStaticPaths } from "next";
 import { useRouter } from "next/router"
+import { pokeApi } from "../../api";
 import { Layout } from "../../components/layouts"
+import { Pokemon } from "../../interfaces";
 
-const PokemonPage = () => {
+interface Props {
+  pokemon: Pokemon;
+}
+
+const PokemonPage: NextPage<Props> = ({pokemon}) => {
 
   // Esto podria usarse pero se usará otra forma
   // const router = useRouter()
   // console.log(router.query)
-
   return (
     <Layout title='Algun pokemon'>
-        <h1>Hola mundo</h1>
+        <h1>{pokemon.name}</h1>
     </Layout>
   )
 }
+
+
+// You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes => [id].tsx
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  const pokemons151 = [...Array(151)].map((value, index) => `${index + 1}`)
+  
+  return {
+    //Asi se generan las paginas independientes 1, 2 y 3 al momento de yarn build
+    // paths: [
+    //   {
+    //     params: { id: '1' }
+    //   },
+    //   {
+    //     params: { id: '2' }
+    //   },
+    //   {
+    //     params: { id: '3' }
+    //   },
+    // ],
+    paths: pokemons151.map( id => ({
+      params: { id }
+    })),
+    fallback: false //false manda un 404 cuando el path no coincide el id esperado.
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({params}) => {
+  //Cuando se ejecutan los paths, pasa a ejecutar staticprops. Ahora necesitamos el argumento desde el contexto con ctx.params
+  const { id } = params as {id: string};
+
+  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${ id }`)
+
+  return {
+    props: {
+      pokemon: data
+    }
+  }
+}
+
 
 export default PokemonPage
