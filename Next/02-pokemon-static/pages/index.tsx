e7dfa08b4f8e107a8a0924de3487382f8a4861ca
Uriel Soubran <uriel_bee15@hotmail.com>
2022-07-30 09:49:45 -0500

    Nextui Cards
    So it works a bit like bootstrap, sort of.
    
    It's straight forward.
    Instead of using ul and li, we use the cards.
    
    Also, according to Fernando Herrera, it implements lazy loading, so the cards will be rendered once it knows it's about to enter the viewport.
    I don't know how to actually assert that statement, but ... well, I guess we believe Nextui devs or Fernando Herrera.
    
    Also, he uses in the video the properties hoverable and clickeable.
    By the time I'm doing the course, those properties changed their names to isHoverable and isPressable.
    
    More on that in:
    https://nextui.org/docs/components/card

--- Next/02-pokemon-static/pages/index.tsx
+++ Next/02-pokemon-static/pages/index.tsx
@@ -1,8 +1,8 @@
 import { NextPage, GetStaticProps } from 'next'
-import { pokeApi } from '../api'
+import { Card, Grid, Row, Text } from '@nextui-org/react'
 
+import { pokeApi } from '../api'
 import { Layout } from '../components/layouts'
-import { Button } from '@nextui-org/react'
 import { PokemonListResponse, SmallPokemon } from '../interfaces'
 
 
@@ -11,18 +11,32 @@ interface Props {
 }
 
 const HomePage: NextPage<Props> = ({pokemons}) => {
-  console.log(pokemons)
+  
   return (
     <Layout title='Listado de Pokemons'>
-      <ul>
+      <Grid.Container gap={ 2 } justify='flex-start'>
         {
-          pokemons.map(({id, name}) => (
-            <li key={ id }>
-              #{ id } - { name }
-            </li>
+          pokemons.map(({id, name, img}) => (
+            <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+              <Card isHoverable isPressable>
+                <Card.Body css={{p: 1}}>
+                  <Card.Image 
+                    src={img}
+                    width='100%'
+                    height={140}
+                  />
+                </Card.Body>
+                <Card.Footer>
+                  <Row justify='space-between'>
+                    <Text transform='capitalize'>{name}</Text>
+                    <Text>#{id}</Text>
+                  </Row>
+                </Card.Footer>
+              </Card>
+            </Grid>
           ))
         }
-      </ul>
+      </Grid.Container>
     </Layout>
   )
 }
