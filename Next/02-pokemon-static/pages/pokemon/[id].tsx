0e5cd2b093546740502d86d3003d4a091f763350
Uriel Soubran <uriel_bee15@hotmail.com>
2022-08-01 15:52:14 -0500

    Confetti

--- Next/02-pokemon-static/pages/pokemon/[id].tsx
+++ Next/02-pokemon-static/pages/pokemon/[id].tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
+
 import { NextPage, GetStaticProps, GetStaticPaths } from "next";
 import { Button, Card, Container, Grid, Image, Text } from "@nextui-org/react";
+
+import confetti from 'canvas-confetti'
 
 import { pokeApi } from "../../api";
 import { Layout } from "../../components/layouts"
@@ -25,6 +28,18 @@ const PokemonPage: NextPage<Props> = ({pokemon}) => {
   const onToggleFavorite = () => {
     localFavorites.toggleFavorite( pokemon.id )
     setIsInFavorites( !isInFavorites )
+    if( isInFavorites ) return;
+
+    confetti({
+      zIndex: 999,
+      particleCount: 100,
+      spread: 160,
+      angle: -100,
+      origin: {
+        x: 1,
+        y: 0
+      }
+    })
   }
 
   return (
